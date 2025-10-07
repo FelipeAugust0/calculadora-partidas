7196cf3364f45d24677be42e45b567b05b77d278
Felipe Augusto da Silva <lipe.augusto2013@gmail.com>
2025-10-07 16:00:54 -0300

    Update CalculadoraPartidas.js

---
+++ CalculadoraPartidas.js
@@ -0,0 +1,24 @@
+function calcularNivel(vitorias, derrotas){
+    let saldo = vitorias - derrotas;
+    let nivel = "";
+
+    if (saldo < 10) {
+    nivel = "Ferro";
+  } else if (saldo >= 11 && saldo <= 20) {
+    nivel = "Bronze";
+  } else if (saldo >= 21 && saldo <= 50) {
+    nivel = "Prata";
+  } else if (saldo >= 51 && saldo <= 80) {
+    nivel = "Ouro";
+  } else if (saldo >= 81 && saldo <= 90) {
+    nivel = "Diamante";
+  } else if (saldo >= 91 && saldo <= 100) {
+    nivel = "Lendário";
+  } else if (saldo >= 101) {
+    nivel = "Imortal";
+  }
+}
+
+return("O saldo de vitórias do herói é: " + saldo + ". Portanto o herói é nível: " + nivel)
+
+console.log(calcularNivel(87, 23))
